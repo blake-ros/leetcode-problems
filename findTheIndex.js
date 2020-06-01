8c36de1193df7171174c9bccd173670d7fcdbfd6
Blake Ros <blakerosdodger@yahoo.com>
2020-05-31 22:44:24 -0700

    completed finding the index problem

---
+++ findTheIndex.js
@@ -0,0 +1,27 @@
+// Question :
+
+// Find The Index
+
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+//   Examples
+// findIndex(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// findIndex(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// findIndex(["a", "g", "y", "d"], "d") ➞ 3
+
+// findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+
+
+
+
+
+
+
+
+// Answer:
+
+function find_index(arr, str) {
+  return arr.indexOf(str);
+}
