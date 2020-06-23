6d3d0ea3901052a5190a6cb795f26f10dfa34fa1
Blake Ros <blakerosdodger@yahoo.com>
2020-06-22 19:21:24 -0700

    completed add consecutive numbers problem

---
+++ addConsecutiveNumbers.js
@@ -0,0 +1,26 @@
+// Question:
+
+Add a Consecutive List of Numbers
+Write a function that takes the last number of a consecutive list of numbers and returns the total of all numbers up to and including it.
+
+  Examples
+addUpTo(3) ➞ 6
+// 1 + 2 + 3 = 6
+
+addUpTo(10) ➞ 55
+// 1 + 2 + 3 + ... + 10 = 55
+
+addUpTo(7) ➞ 28
+// 1 + 2 + 3 + ... + 7 = 28
+
+
+// Answer:
+
+function addUpTo(n) {
+  let newNum = n
+  while (n > 0) {
+    n--;
+    newNum += n;
+  }
+  return newNum
+}
