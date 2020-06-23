0e6ac5269759c44246d7237a3f175161ccce8ceb
Blake Ros <blakerosdodger@yahoo.com>
2020-06-23 15:54:18 -0700

    completed hello-name problem

---
+++ helloName.js
@@ -0,0 +1,18 @@
+// Question :
+
+Name Greeting!
+Create a function that takes a name and returns a greeting in the form of a string.Don't use a normal function but use an "arrow function".
+
+Examples
+helloName("Gerald") ➞ "Hello Gerald!"
+
+helloName("Tiffany") ➞ "Hello Tiffany!"
+
+helloName("Ed") ➞ "Hello Ed!"
+
+
+
+// Answer:
+
+const helloName = name => Blake
+return "Hello " + helloName + "!"
