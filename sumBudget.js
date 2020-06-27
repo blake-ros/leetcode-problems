fd13f244749125ecb1d0ba59fcc69cac4ae536a5
Blake Ros <blakerosdodger@yahoo.com>
2020-06-26 19:54:57 -0700

    completed sum of peoples budget exercise

---
+++ sumBudget.js
@@ -0,0 +1,26 @@
+// Question:
+
+// Get Sum of People's Budget
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+
+// Examples
+// getBudgets([
+//   { name: "John", age: 21, budget: 23000 },
+//   { name: "Steve", age: 32, budget: 40000 },
+//   { name: "Martin", age: 16, budget: 2700 }
+// ]) ➞ 65700
+
+// getBudgets([
+//   { name: "John", age: 21, budget: 29000 },
+//   { name: "Steve", age: 32, budget: 32000 },
+//   { name: "Martin", age: 16, budget: 1600 }
+// ]) ➞ 62600
+
+
+
+// Answer:
+
+function getBudgets(arr) {
+  const result = arr.reduce((cur, acc) => cur + acc.budget, 0);
+  return result
+}
