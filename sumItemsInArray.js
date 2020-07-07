d9709c4a1dc1fd00c580008b918a4c568d857a25
Blake Ros <blakerosdodger@yahoo.com>
2020-07-07 11:35:06 -0700

    updated solution

---
+++ sumItemsInArray.js
@@ -0,0 +1,24 @@
+// Question:
+
+// Sum of the Items in an Array
+// Create a function that takes an array and returns the sum of all items in the array.
+
+//   Examples
+// sumArray([1, 2, 3]) ➞ 6
+// // 1 + 2 + 3 = 6
+
+// sumArray([1, [2, [1]], 3]) ➞ 7
+// // 1 + 2 + 1 + 3 = 7
+
+
+
+
+
+
+
+// Answer:
+
+function sumArray(arr) {
+  const newArray = arr.flat(3);
+  return newArray.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
