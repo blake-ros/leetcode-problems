7e6045b54b4f1c233795a0584ae82abf80b5743e
Blake Ros <blakerosdodger@yahoo.com>
2020-09-14 09:11:36 -0700

    completed emotify problem

---
+++ emotify.js
@@ -0,0 +1,33 @@
+// Question:
+
+Emotify Sentence
+Create a function that changes specific words into emoticons.Given a sentence as a string, replace the words smile, grin, sad and mad with their corresponding emoticons.
+
+word	emoticon
+smile: D
+grin:)
+sad: (
+  mad: P
+Examples
+emotify("Make me smile") ➞ "Make me :D"
+
+emotify"Make me grin" ➞ "Make me :)"
+
+emotify("Make me sad") ➞ "Make me :("
+
+
+
+// Answer:
+
+function emotify(str) {
+  let [first, second, last] = str.split(" ");
+
+  let emotify = {
+    smile: ":D",
+    grin: ":)",
+    sad: ":(",
+    mad: ":P"
+  }
+
+  return `Make me ${emotify[last]}`;
+}
