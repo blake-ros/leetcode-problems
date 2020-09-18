e2940180e1dac7cd1816765c6f422161fcd9f25c
Blake Ros <blakerosdodger@yahoo.com>
2020-09-18 10:40:05 -0700

    completed count words problem

---
+++ countWords.js
@@ -0,0 +1,20 @@
+// Question:
+
+Get Word Count
+Create a function that takes a string and returns the word count.The string will be a sentence.
+
+  Examples
+countWords("Just an example here move along") ➞ 6
+
+countWords("This is a test") ➞ 4
+
+countWords("What an easy task, right") ➞ 5
+
+
+
+// Answer:
+
+function countWords(str) {
+  const words = str.split(' ').length;
+  return words
+}
