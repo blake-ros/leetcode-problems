453ae9b6e03b7202ef95001d4d1e275ccbdd8ef2
Blake Ros <blakerosdodger@yahoo.com>
2020-09-26 10:26:13 -0700

    completed paths problem

---
+++ paths.js
@@ -0,0 +1,36 @@
+// Question:
+
+Travelling Salesman Problem
+A salesman has a number of cities to visit.They want to calculate the total number of possible paths they could take, visiting each city once before returning home.Return the total number of possible paths a salesman can travel, given n cities.
+
+If we have cities A, B and C, possible paths would be:
+
+A -> B -> C
+A -> C -> B
+B -> A -> C
+B -> C -> A
+C -> B -> A
+C -> A -> B
+...which gives us 6 as the possible number of paths.
+
+  Examples
+paths(4) ➞ 24
+
+paths(1) ➞ 1
+
+paths(9) ➞ 362880
+
+
+
+
+// Answer:
+
+function paths(n) {
+  if (n < 0) {
+    return 0
+  } else if (n === 0) {
+    return 1
+  } else {
+    return n * paths(n - 1)
+  }
+}
