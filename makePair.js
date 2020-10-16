d64e7eddd83f9b650fcebf7d985a4041bfffe0b0
Blake Ros <blakerosdodger@yahoo.com>
2020-10-15 21:34:44 -0700

    completed make pari problem

---
+++ makePair.js
@@ -0,0 +1,21 @@
+// Question:
+
+Pair Management
+Given two arguments, return an array which contains these two arguments.
+
+  Examples
+makePair(1, 2) ➞[1, 2]
+
+makePair(51, 21) ➞[51, 21]
+
+makePair(512124, 215) ➞[512124, 215]
+
+
+
+
+// Answer:
+
+function makePair(num1, num2) {
+  const myArray = [num1, num2]
+  return myArray
+}
