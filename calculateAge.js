e9525533a5fb5a6c3758daf661b6699adeb575ce
Blake Ros <blakerosdodger@yahoo.com>
2020-10-22 19:08:12 -0700

    completed calculate age problem

---
+++ calculateAge.js
@@ -0,0 +1,21 @@
+// Question:
+
+Convert Age to Days
+Create a function that takes the age and return the age in days.
+
+  Examples
+calcAge(65) ➞ 23725
+
+calcAge(0) ➞ 0
+
+calcAge(20) ➞ 7300
+
+
+
+
+// Answer:
+
+function calcAge(age) {
+  const days = age * 365;
+  return days
+}
