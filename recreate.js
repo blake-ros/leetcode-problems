0ebb06cecb652f3255d6f0f0f422b120173efdad
blake-ros <dev@MSI.localdomain>
2020-12-21 20:37:35 -0800

    completed problem

---
+++ recreate.js
@@ -0,0 +1,24 @@
+// Question:
+
+Recreating the String.length Property
+Create a function which returns the length of a string, WITHOUT using String.length property.
+
+  Examples
+length("Hello World") ➞ 11
+
+length("Edabit") ➞ 6
+
+length("wash your hands!") ➞ 16
+
+
+
+
+// Answer:
+
+function length(s) {
+  var count = 0;
+  for (let i = 0; i < s.length; i++) {
+    count++
+  }
+  return count
+}
