c80ed78b9179c67769d924c0dfa284249d22221d
blake-ros <dev@MSI.localdomain>
2021-01-22 18:39:36 -0800

    added a jsx variable buttonText

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = () => {
+  const buttonText = "Click Me!"
   return (
       <div>
         <label className="label" for="name">
           Enter name:
         </label>
         <input id="name" type="text" />
-        <button style={{backgroundColor: "blue", color: "white"}}>Submit</button>
+        <button style={{backgroundColor: "blue", color: "white"}}>{buttonText}</button>
       </div>
   );
 };
