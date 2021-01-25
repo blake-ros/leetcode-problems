fd5264e51cf677a18ebbde26c8655c1bd6349fca
blake-ros <dev@MSI.localdomain>
2021-01-25 13:44:07 -0800

    added conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,17 +6,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
   }
 
   // React says we have to define render!!
   render() {
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-      );
 
-      return <div>Latitude: </div>
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>
+    }
+
+    if(!this.state.errorMesage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+      // return (
+      //   <div>
+      //     Latitude: {this.state.lat}
+      //     <br />
+      //     Error: {this.state.errorMessage}
+      //   </div>
+      // );
   }
 }
 
