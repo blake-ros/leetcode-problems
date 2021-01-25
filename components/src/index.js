0a08b680758bad2d294fe471e0d39306bb5f62e9
blake-ros <dev@MSI.localdomain>
2021-01-25 10:17:46 -0800

    passed approval card the commentdetail component through props:

--- components/src/index.js
+++ components/src/index.js
@@ -6,8 +6,10 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" userText="Nice Post" userAvatar="https://source.unsplash.com/random" />
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" userText="Nice Post" userAvatar="https://source.unsplash.com/random" />
+      </ApprovalCard>
+
       <CommentDetail author="Alex" timeAgo="Today at 2:30AM" userText="Same To You!" userAvatar="https://source.unsplash.com/random" />
       <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" userText="Awesome Thread" userAvatar="https://source.unsplash.com/random" />
     </div>
