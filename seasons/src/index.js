cf25280bb6bc5d12058e9560521788816d8dbe0c
blake-ros <dev@MSI.localdomain>
2021-01-28 11:48:45 -0800

    added loading spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -43,7 +44,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>
+    return <Spinner />;
       // return (
       //   <div>
       //     Latitude: {this.state.lat}
