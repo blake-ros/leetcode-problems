425c3c75647f67b262b6e4413fa7b1e342831286
blake-ros <dev@MSI.localdomain>
2021-01-27 20:57:21 -0800

    completed edabit problem

---
+++ makeNewPair.js
@@ -0,0 +1,22 @@
+// Question:
+
+
+
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+//   Examples
+// makePair(1, 2) ➞[1, 2]
+
+// makePair(51, 21) ➞[51, 21]
+
+// makePair(512124, 215) ➞[512124, 215]
+
+
+
+// Answer:
+
+
+function makePair(num1, num2) {
+  return [num1, num2]
+}
