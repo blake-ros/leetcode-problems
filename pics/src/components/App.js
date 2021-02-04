4705b850969ff8a460edca519b7f0c0f1dc76c6f
blake-ros <dev@MSI.localdomain>
2021-02-04 13:14:28 -0800

    added unsplash api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID z4BCreM2_TSU2dJnS5ol8VDeHD0xBjg2Wf_KHMnmoz4'
+      }
+    });
+    console.log(term);
+  }
+
+  render() {
+    return(
+      <div className = "ui container" style = {{ marginTop: '10px'}}>
+    <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div >
+    );
+  }
+}
+
+
 
 export default App;
